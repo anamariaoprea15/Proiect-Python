3fe1e6e5463c1d1efb59182768e20cb1cc75b86d
Oprea Ana-Maria <anamariaoprea15@gmail.com>
2021-12-15 00:16:14 +0200

    db functions

--- TopForbes/main.py
+++ TopForbes/main.py
@@ -38,6 +38,48 @@ def insert_row(conn, values):
     return cur.lastrowid
 
 
+def top_10_yougest(conn):
+    stmt = '''SELECT personName, age FROM billionaires
+                WHERE age IS NOT NULL
+                ORDER BY age LIMIT 10'''
+
+    cur = conn.cursor()
+    cur.execute(stmt)
+
+    rows = cur.fetchall()
+
+    print("Top 10 youngest billionaires")
+    for row in rows:
+        print(row)
+
+
+def count_american_citizenship(conn):
+
+    stmt = '''SELECT COUNT(*) FROM billionaires
+            WHERE countryOfCitizenship LIKE 'United States' '''
+
+    cur = conn.cursor()
+    cur.execute(stmt)
+
+    count = cur.fetchone()
+
+    print("Billionaires with american citizenship:", count[0])
+    print("Billionaires with other citizenship:", 200-count[0])
+
+def top_10_philantropic_score(conn):
+    stmt = '''SELECT personName, philanthropyScore FROM billionaires
+                    WHERE philanthropyScore IS NOT NULL
+                    ORDER BY philanthropyScore DESC LIMIT 10'''
+
+    cur = conn.cursor()
+    cur.execute(stmt)
+
+    rows = cur.fetchall()
+
+    print("Top 10 by philantropic score: ")
+    for row in rows:
+        print(row)
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 
@@ -61,13 +103,18 @@ if __name__ == '__main__':
         resp = urllib.request.urlopen(req)
         data = resp.read().decode('utf-8')
         json_obj = json.loads(data)
-        #dictionary with list of billionaires and total count
+        # dictionary with list of billionaires and total count
         person_list_dict = json_obj["personList"]
         # list of billionaires (every billionaire is a dictionary)
-        persons_lists = person_list_dict["personsLists"] #list of billionaires
+        persons_lists = person_list_dict["personsLists"]  # list of billionaires
         for p in persons_lists:
-            values_to_insert = (p['rank'], p['personName'], p.get('age'), p['country'], p['countryOfCitizenship'], p.get('philanthropyScore'), p['finalWorth'])
+            values_to_insert = (p.get('rank'), p.get('personName'), p.get('age'), p.get('country'),
+                                p.get('countryOfCitizenship'), p.get('philanthropyScore'), p.get('finalWorth'))
             insert_row(con, values_to_insert)
+
+        top_10_yougest(con)
+        count_american_citizenship(con)
+        top_10_philantropic_score(con)
 
     except Exception as e:
         print("Error : ", e)
