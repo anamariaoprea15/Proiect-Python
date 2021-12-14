ab5c0e93e02d4b5b0448a82d9cea6f985d3a2907
Oprea Ana-Maria <anamariaoprea15@gmail.com>
2021-12-14 23:30:33 +0200

    insert into db

--- TopForbes/main.py
+++ TopForbes/main.py
@@ -5,7 +5,7 @@ import sqlite3
 
 headers = {'cookie': 'notice_gdpr_prefs'}  # required cookie that needs to be sent to view the data
 
-url_forbes = r"https://www.forbes.com/forbesapi/person/billionaires/2021/position/true.json?limit=2"
+url_forbes = r"https://www.forbes.com/forbesapi/person/billionaires/2021/position/true.json?limit=200"
 
 
 def create_connection(db_file):
@@ -29,8 +29,33 @@ def create_table(conn, statement):
         print(ex)
 
 
+def insert_row(conn, values):
+    stmt = ''' INSERT INTO billionaires(rank, personName, age, country, countryOfCitizenship, philanthropyScore, finalWorth)
+                 VALUES(?,?,?,?,?,?,?) '''
+    cur = conn.cursor()
+    cur.execute(stmt, values)
+    conn.commit()
+    return cur.lastrowid
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+
+    con = create_connection('forbes.db')
+    cursor = con.cursor()
+    cursor.execute("DROP TABLE billionaires")
+    stmt = """ CREATE TABLE IF NOT EXISTS billionaires (
+                                            rank integer NOT NULL,
+                                            personName text NOT NULL,
+                                            age integer,
+                                            country text NOT NULL,
+                                            countryOfCitizenship text NOT NULL,
+                                            philanthropyScore integer,
+                                            finalWorth real NOT NULL
+                                        ); """
+
+    create_table(con, stmt)
+
     try:
         req = urllib.request.Request(url_forbes, None, headers)
         resp = urllib.request.urlopen(req)
@@ -41,19 +66,8 @@ if __name__ == '__main__':
         # list of billionaires (every billionaire is a dictionary)
         persons_lists = person_list_dict["personsLists"] #list of billionaires
         for p in persons_lists:
-            print(p)
+            values_to_insert = (p['rank'], p['personName'], p.get('age'), p['country'], p['countryOfCitizenship'], p.get('philanthropyScore'), p['finalWorth'])
+            insert_row(con, values_to_insert)
+
     except Exception as e:
         print("Error : ", e)
-
-    con = create_connection('forbes.db')
-    stmt = """ CREATE TABLE IF NOT EXISTS billionaires (
-                                        rank integer PRIMARY KEY,
-                                        personName text NOT NULL,
-                                        age integer NOT NULL,
-                                        country text NOT NULL,
-                                        countryOfCitizenship text NOT NULL,
-                                        philanthropyScore integer NOT NULL
-                                    ); """
-
-    create_table(con, stmt)
-
